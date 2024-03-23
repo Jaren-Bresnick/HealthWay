709e2c6d183951beb7e54f370e5537f526e793ce
Rishi Sureshkumar <123980270+Rishi-Sureshkumar@users.noreply.github.com>
2024-03-23 18:15:28 -0400

    idk man

--- food-recognition/stocking_recognition.py
+++ food-recognition/stocking_recognition.py
@@ -1,5 +1,5 @@
 import food_recognition
 
 # Call the function with the relative path to the image file
-food_recognition.main("R:/HooHacks/FoodInventory/food-recognition/images/test/tomatocarrot.jpg")
+food_recognition.main("images/test/tomatocarrot.jpg")
 
