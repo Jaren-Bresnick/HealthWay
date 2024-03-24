651d0f7c1e8f921df09368eedc1303d3512ac277
Rishi Sureshkumar <123980270+Rishi-Sureshkumar@users.noreply.github.com>
2024-03-24 00:28:55 -0400

    Fixed imports for image_process.py

--- apis/image_process_api.py
+++ apis/image_process_api.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import food_recognition
-import receipt_recognition
-import stocking_recognition
+from pathlib import Path
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from food_recognition.food_recognition import main as food_recognition
+from food_recognition.receipt_recognition import main as receipt_recognition
+from food_recognition.stocking_recognition import main as stocking_recognition
+from pills_recognition.pills_recognition import main as pills_recognition
 import inventory_api
 
 app = FastAPI()
@@ -11,6 +16,13 @@ class Item(BaseModel):
     product: str
     quantity: int
     userid: str
+
+class Pill(BaseMode1):
+    product: str
+    dosage: str
+    quantity: int
+    refill_date: str
+    expiration_date: str
 
 class ImageData(BaseModel):
     type_of_image: str
@@ -26,6 +38,8 @@ def process_image(data: ImageData):
         json_file = receipt_recognition.identify_food_in_image(image_path)
     elif type_of_image == "pantry":
         json_file = food_recognition.identify_food_in_image(image_path)
+    elif type_of_image == "pills":
+        json_file = pills_recognition.identify_pills_in_image(image_path)
     elif type_of_image == "stocking":
         json_file = stocking_recognition.main()
     else:
@@ -35,10 +49,20 @@ def process_image(data: ImageData):
     if not json_file or "food_items" not in json_file or not json_file["food_items"]:
         raise HTTPException(status_code=400, detail="No food items found or incorrect JSON structure.")
 
+    if type_of_image == "pills":
+        pill = Pill(product=json_file["pills"][0]["name"],
+                    dosage=json_file["pills"][0]["dosage"],
+                    quantity=json_file["pills"][0]["quantity"],
+                    refills=json_file["pills"][0]["refill_date"],
+                    expiration_date=json_file["pills"][0]["expiration_date"])
+
+        inventory_api.add_or_update_pill(pill)
+
     # Convert json file output to an 'item' object and take quantity from json file
-    item = Item(product=json_file["food_items"][0]["name"],
-                quantity=json_file["food_items"][0]["quantity"],
-                userid="1")
+    elif type_of_image in ["receipt", "pantry", "stocking"]:
+        item = Item(product=json_file["food_items"][0]["name"],
+                    quantity=json_file["food_items"][0]["quantity"],
+                    userid="1")
 
     # If the image is a receipt or pantry, add/update the item to inventory
     # If the image is stocking, the action should be determined by the main function of stocking_recognition
