84ffc4c581f0ec748eabbdae49258ddb386b3818
Rishi Sureshkumar <123980270+Rishi-Sureshkumar@users.noreply.github.com>
2024-03-24 01:13:01 -0400

    Fixed appends for routes.py

--- apis/image_process_api.py
+++ apis/image_process_api.py
@@ -3,12 +3,13 @@ from pydantic import BaseModel
 from pathlib import Path
 import sys
 import os
-sys.path.append(os.path.abspath('..'))
-import food_recognition.food_recognition
-import food_recognition.receipt_recognition
-import food_recognition.motion_recognition
-import food_recognition.stocking_recognition
-import pills_recognition.pills_recognition
+sys.path.append(os.path.abspath('../food_recognition'))
+sys.path.append(os.path.abspath('../pills_recognition'))
+import food_recognition
+import receipt_recognition
+import motion_recognition
+import stocking_recognition
+import pills_recognition
 
 app = FastAPI()
 
